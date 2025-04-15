53f151e419dc9b3336827dc7385dda76fe3c0419
Ivan <stupakvana2@gmail.com>
2025-04-15 17:37:15 +0300

    add interfaces and impl in domain

--- data/database/impl/src/main/java/dev/stupak/database/impl/source/dao/AlertsDao.kt
+++ data/database/impl/src/main/java/dev/stupak/database/impl/source/dao/AlertsDao.kt
@@ -1,15 +1,14 @@
-package com.example.impl.source.dao
+package dev.stupak.database.impl.source.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.database.core.model.AlertDatabaseModel
+import dev.stupak.database.core.model.AlertDatabaseModel
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AlertsDao {
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAlerts(alerts: List<AlertDatabaseModel>)
 
--- presentation/feature/widget/src/main/java/com/example/widget/components/AlertListWidget.kt
+++ presentation/feature/widget/src/main/java/com/example/widget/components/AlertListWidget.kt
@@ -1,17 +1,13 @@
 package com.example.widget.components
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.layout.fillMaxWidth
 import com.example.widget.model.AlertsUiModel
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AlertListWidget(alerts: List<AlertsUiModel>) {
-
     LazyColumn(modifier = GlanceModifier.fillMaxWidth()) {
         items(alerts.size) { index ->
             val alert = alerts[index]
